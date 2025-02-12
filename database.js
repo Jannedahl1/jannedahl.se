b03857ac78742d74455dcd91345cf40e69e33d14
Janne <janne.dahl92@gmail.com>
2025-02-12 21:17:57 +0100

    HiddenAPI

--- database.js
+++ database.js
@@ -1,11 +1,15 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCOq9E1qaHjQ-x2molz22sITMPdhR7dXks",
-    authDomain: "jannedahl-55349.firebaseapp.com",
-    projectId: "jannedahl-55349",
-    storageBucket: "jannedahl-55349.firebasestorage.app",
-    messagingSenderId: "711435268575",
-    appId: "1:711435268575:web:fea31561a27057be2a946a"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
