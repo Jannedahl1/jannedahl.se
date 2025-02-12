e25d215641ec1a5b5f2ca7ceecc3867098cd1a9a
Jannedahl1 <janne.dahl92@gmail.com>
2025-02-12 17:02:58 +0100

    Stuff

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const aboutSection = document.getElementById("about");
+    aboutSection.addEventListener("click", function() {
+        alert("You clicked on the About section!");
+    });
+});
